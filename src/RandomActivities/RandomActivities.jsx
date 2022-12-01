3c987240a90a9ec005adda81b6112be810923ab2
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-12-01 17:51:33 +0000

    finished jsx

--- src/RandomActivities/RandomActivities.jsx
+++ src/RandomActivities/RandomActivities.jsx
@@ -5,9 +5,14 @@ const RandomActivities = () => {
   const [activites, setActivites] = useState('');
 
   return (
-    <div className='flex flex-col items-center justify-center my-auto h-screen text-center'>
-      <div className='border-2 border-white rounded-md p-10 '>
-        <p className='border-2 border-white font-serif p-2'>{activites}sd</p>
+    <div className='flex flex-col items-center justify-center text-center'>
+      <h1 className='text-2xl font-serif font-bold mt-36'>
+        Want A New Activity?
+      </h1>
+      <div className='border-2 border-white rounded-md p-10 mt-44'>
+        <p className='border-2 border-white font-serif p-2 font-bold'>
+          {activites}dummy text
+        </p>
         <button className='border-2 border-white p-3 rounded-lg mt-10 font-bold font-serif'>
           Click For Activities
         </button>
