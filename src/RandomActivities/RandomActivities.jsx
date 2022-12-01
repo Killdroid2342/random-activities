587b2ff2b1bf80bbc44ccb6b12ae6bd1e3aff112
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-12-01 17:44:49 +0000

    editied jsx styling

--- src/RandomActivities/RandomActivities.jsx
+++ src/RandomActivities/RandomActivities.jsx
@@ -7,8 +7,8 @@ const RandomActivities = () => {
   return (
     <div className='flex flex-col items-center justify-center my-auto h-screen text-center'>
       <div className='border-2 border-white rounded-md p-10 '>
-        <p className='border-2 border-white'>asd</p>
-        <button className='border-2 border-white p-3 rounded-lg mt-10'>
+        <p className='border-2 border-white font-serif p-2'>{activites}sd</p>
+        <button className='border-2 border-white p-3 rounded-lg mt-10 font-bold font-serif'>
           Click For Activities
         </button>
       </div>
