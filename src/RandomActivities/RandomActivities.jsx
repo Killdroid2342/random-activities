2aad80a56de37546d4a4b6944b7c2b95c4d9c156
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-12-01 17:33:55 +0000

    simple jsx

--- src/RandomActivities/RandomActivities.jsx
+++ src/RandomActivities/RandomActivities.jsx
@@ -1,9 +1,14 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 const RandomActivities = () => {
+  const [activites, setActivites] = useState('');
+
   return (
-    <div>
-      <p>asd</p>
+    <div className='flex flex-col items-center'>
+      <div className='border-2 border-white rounded-md p-10'>
+        <p className='border-2 border-white'>asd</p>
+      </div>
     </div>
   );
 };
